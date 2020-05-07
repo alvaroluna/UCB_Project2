374bd8fe4348e77ad1f352c7a2caa6770b22ff2b
alvaroluna <luna.alvaro3@gmail.com>
2020-05-06 20:09:03 -0700

    Used lint and fix to correct formatting

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,13 @@
+// think about the pages you want displayed, not the data to display on them
+
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   ////////////////////////////
   // Load index page - HOME //
   ////////////////////////////
-  app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -16,39 +18,23 @@ module.exports = function (app) {
   ////////////////////////////////////////////////////
   // Load example page and pass in an example by id //
   ////////////////////////////////////////////////////
-  app.get("/example/:id", function (req, res) {
+  app.get("/example/:id", function(req, res) {
     db.Example.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function (dbExample) {
+    }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
 
-  //////////////////////////////
-  // Load main volunteer page //
-  //////////////////////////////
-  app.get("/volunteer/:id", function (req, res) {
-    db.Example.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (dbExample) {
-      res.render("volunteer", {
-        example: dbExample
-      });
-    });
-  });
-
-
   //////////////////////////////////
   // Load dog walk volunteer page //
   //////////////////////////////////
-  app.get("/dogWalkVolunteer", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/dogWalkVolunteer", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("dogWalkVolunteer", {
         msg: "Welcome!",
         examples: dbExamples
@@ -56,10 +42,25 @@ module.exports = function (app) {
     });
   });
 
+  //////////////////////////////
+  // Load main volunteer page //
+  //////////////////////////////
+  app.get("/volunteer/:id", function(req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbExample) {
+      res.render("volunteer", {
+        example: dbExample
+      });
+    });
+  });
+
   //////////////////////////////////////////////
   // Render 404 page for any unmatched routes //
   //////////////////////////////////////////////
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
