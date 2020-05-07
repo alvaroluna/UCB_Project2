a47d3a20ed53ac03d44bc025aad4d1b5904212a9
alvaroluna <luna.alvaro3@gmail.com>
2020-05-06 18:36:23 -0700

    Map functional and some html routes have been made

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,11 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+module.exports = function (app) {
+  ////////////////////////////
+  // Load index page - HOME //
+  ////////////////////////////
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -11,17 +13,53 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  ////////////////////////////////////////////////////
+  // Load example page and pass in an example by id //
+  ////////////////////////////////////////////////////
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  //////////////////////////////
+  // Load main volunteer page //
+  //////////////////////////////
+  app.get("/volunteer/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
+      res.render("volunteer", {
+        example: dbExample
+      });
+    });
+  });
+
+
+  //////////////////////////////////
+  // Load dog walk volunteer page //
+  //////////////////////////////////
+  app.get("/dogWalkVolunteer", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("dogWalkVolunteer", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+  //////////////////////////////////////////////
+  // Render 404 page for any unmatched routes //
+  //////////////////////////////////////////////
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
