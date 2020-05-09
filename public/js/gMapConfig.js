b098ee598264468dea3175901da4216431be1f6b
alvaroluna <luna.alvaro3@gmail.com>
2020-05-08 19:00:11 -0700

    Got map to init at current location and changed icon...several other stuff before but I staged the wrong file

--- public/js/gMapConfig.js
+++ public/js/gMapConfig.js
@@ -4,42 +4,56 @@
 // locate you.
 var map, infoWindow;
 
+// initMap() is called by src callback in handlebars file
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-    zoom: 6
-  });
-  infoWindow = new google.maps.InfoWindow();
-
-  // Try HTML5 geolocation.
+  // Try HTML5 geolocation. - WHERE IS THE NAVIGATOR OBJECT COMING FROM???
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       function (position) {
-        var pos = {
+
+        // get current location
+        var currentPos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("Location found.");
+
+        //////////////////////////////////////////////////////////// THIS CAN BE ABOVE IF
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: currentPos,
+          zoom: 15
+        });
+        infoWindow = new google.maps.InfoWindow();
+        //////////////////////////////////////////////////////////// THIS CAN BE ABOVE IF
+
+
+        // this is an info window but don't really want it
+        // infoWindow.setPosition(currentPos);
+        // infoWindow.setContent("Location found.");
+
         infoWindow.open(map);
-        map.setCenter(pos);
+        map.setCenter(currentPos);
+
+        // custom dog icon at geolocation
+        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+        var marker = new google.maps.Marker({
+          map: map,
+          position: currentPos,
+          icon: iconBase + 'parking_lot_maps.png'
+        })
+
       },
       function () {
         handleLocationError(true, infoWindow, map.getCenter());
-      }
-    );
+      });
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
+function handleLocationError(browserHasGeolocation, infoWindow, currentPos) {
+  infoWindow.setPosition(currentPos);
   infoWindow.setContent(browserHasGeolocation ?
     'Error: The Geolocation service failed.' :
     'Error: Your browser doesn\'t support geolocation.');
