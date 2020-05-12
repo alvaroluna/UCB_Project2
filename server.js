f5315fa44ac62cf72c21252911e6c08001a851cc
Lay84 <Iyan.laylor@gmail.com>
2020-05-12 15:48:18 -0700

    --created helper functions for handlebars so can check task type and render appropriate task card --adusted task model so that task type has string description (dog walking, grooming, vet visit, pet sitting) --created new handle partial for dog walking panel --adjusted main app view with conditions to render task

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 
 var db = require("./models");
-var seniorSeed = require("./seeders/seniorSeed")
-var taskSeed = require("./seeders/taskSeed")
-var volunteerSeed = require("./seeders/volunteerSeed")
+var seniorSeed = require("./seeders/seniorSeed");
+var taskSeed = require("./seeders/taskSeed");
+var volunteerSeed = require("./seeders/volunteerSeed");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -16,10 +16,21 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Handlebars
+
 app.engine(
   "handlebars",
   exphbs({
-    defaultLayout: "main"
+    defaultLayout: "main",
+
+    // TEst
+    helpers: {
+      ifCond: function(v1, v2, options) {
+        if (v1 === v2) {
+          return options.fn(this);
+        }
+        return null;
+      },
+    },
   })
 );
 app.set("view engine", "handlebars");
@@ -38,31 +49,30 @@ if (process.env.NODE_ENV === "test") {
 if (process.env.NODE_ENV === "production") {
   syncOptions.force = false;
 }
- 
 
 // Starting the server, syncing our models -& seeding tables initially------------/
 
-db.sequelize.sync(syncOptions).then(function () {
+db.sequelize.sync(syncOptions).then(function() {
   db.Senior.destroy({
     where: {},
-    truncate: true
-  }).then(function () { 
+    truncate: true,
+  }).then(function() {
     db.Senior.bulkCreate(seniorSeed);
   });
   db.Volunteer.destroy({
     where: {},
-    truncate: true
-  }).then(function () { 
+    truncate: true,
+  }).then(function() {
     db.Volunteer.bulkCreate(volunteerSeed);
   });
   db.Task.destroy({
     where: {},
-    truncate: true
-  }).then(function () { 
-    db.Task.bulkCreate(taskSeed); 
+    truncate: true,
+  }).then(function() {
+    db.Task.bulkCreate(taskSeed);
   });
 
-  app.listen(PORT, function () {
+  app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
@@ -72,4 +82,3 @@ db.sequelize.sync(syncOptions).then(function () {
 });
 
 module.exports = app;
-
